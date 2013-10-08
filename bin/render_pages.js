badda3a7c3374a7d10987dbb28ada7ded9c7f9c8
tangyao <tangyao@taobao.com>
2013-10-08 18:04:13 +0800

    update tab -> space

--- bin/render_pages.js
+++ bin/render_pages.js
@@ -5,13 +5,13 @@ var md = require('markdown').markdown;
 var root = path.resolve(__dirname, '../template/');
 
 fs.readdir(root, function (err, files) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	files.forEach(function (file) {
-		var str = fs.readFileSync(path.resolve(root, file), 'utf8');
-		var convert = md.toHTML(str);
-		fs.writeFile(path.resolve(root, '../', file.replace('.md', '.html')), convert);
-	});
+  if (err) {
+    console.log(err);
+    return;
+  }
+  files.forEach(function (file) {
+    var str = fs.readFileSync(path.resolve(root, file), 'utf8');
+    var convert = md.toHTML(str);
+    fs.writeFile(path.resolve(root, '../', file.replace('.md', '.html')), convert);
+  });
 });
